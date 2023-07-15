d2932369fcde77e795243745739da5335c9330e7
Jxnatan7 <jgabrieldesouzasantos@gmail.com>
2023-07-14 21:04:25 -0300

    :sparkles: Iniciando o projeto e criar o primeiro repositorio (Usuário).

---
+++ Data/SistemaTarefasDBContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Data
+{
+    public class SistemaTarefasDBContext : DbContext
+    {
+        public SistemaTarefasDBContext(DbContextOptions<SistemaTarefasDBContext> options)
+            : base(options)
+        {
+        }
+
+        //Vai criar as nossas tabelas do banco de dados
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> Tarefas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Repositorios/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,13 @@
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Repositorios.Interfaces
+{
+    public interface IUsuarioRepositorio
+    {
+        Task<List<UsuarioModel>> BuscarTodosUsuarios();
+        Task<UsuarioModel> BuscarPorId(int id);
+        Task<UsuarioModel> AdicionarUsuario(UsuarioModel usuario);
+        Task<UsuarioModel> AtualizarUsuario(UsuarioModel usuario, int id);
+        Task<bool> ApagarUsuario(int id);
+    }
+}
